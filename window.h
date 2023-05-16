9af89cfd65cbd263f4b77ca563daf18da17325e9
Lucas <85084337+lukasz-bielerzewski@users.noreply.github.com>
2023-05-16 02:51:33 +0200

    Add files via upload

---
+++ window.h
@@ -0,0 +1,47 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QOpenGLWindow>
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QMatrix4x4>
+#include "transform3d.h"
+
+class QOpenGLShaderProgram;
+
+class Window : public QOpenGLWindow,
+               protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+    // OpenGL Events
+public:
+    Window();
+    ~Window();
+
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void paintGL();
+    void teardownGL();
+
+protected slots:
+    void update();
+
+private:
+    // OpenGL State Information
+    QOpenGLBuffer m_vertex;
+    QOpenGLVertexArrayObject m_object;
+    QOpenGLShaderProgram *m_program;
+
+    // Shader Information
+    int u_modelToWorld;
+    int u_worldToView;
+    QMatrix4x4 m_projection;
+    Transform3D m_transform;
+
+    // Private Helpers
+    void printContextInformation();
+};
+
+#endif // WINDOW_H
